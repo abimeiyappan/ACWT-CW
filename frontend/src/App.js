11ea597bcc55975f4a7ce8a7c1d16833c527a598
gmichail147 <74865205+gmichail147@users.noreply.github.com>
2022-05-03 12:42:03 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import HomePage from './Pages/HomePage'
 
-function App () {
+function App() {
   return (
     <>
-    <Header />
-    <Container>
-    <main className='py-4'>
-      <HomePage />
-    </main>
-    </Container>
-    
-    <Footer />
+      <Header />
+      <Container>
+        <main className='py-4'>
+          <HomePage />
+        </main>
+      </Container>
+
+      <Footer />
     </>
   )
 }
