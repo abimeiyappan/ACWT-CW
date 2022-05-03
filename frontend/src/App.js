ffea9024921404536d460c5fa8835809f2460b33
abi <abi.meiyappan@gmail.com>
2022-05-04 00:30:02 +0100

    Updates upto sec-5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {Container} from 'react-bootstrap'
 import HomePage from './Pages/HomePage'
+import ProductPage from './Pages/ProductPage'
 
-function App () {
+const App = () => {
   return (
-    <>
-    <Header />
-    <Container>
-    <main className='py-4'>
-      <HomePage />
-    </main>
-    </Container>
-    
-    <Footer />
-    </>
+    <Router>
+      <Header />
+      
+        <main className='py-4'>
+          <Container>
+              <Route path = '/' component={HomePage} />
+              <Route path='/product/:id' component={ProductPage} />
+          </Container>
+        </main>
+      
+      <Footer />
+    </Router>
   )
 }
 
