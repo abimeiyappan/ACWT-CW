1867b5a9b705e889f0c5bc14225ffc9077fff1d0
gmichail147 <74865205+gmichail147@users.noreply.github.com>
2022-05-04 23:11:08 +0100

    fixed missing header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,22 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import HomePage from './Pages/HomePage'
 import ProductPage from './Pages/ProductPage'
 
 const App = () => {
   return (
     <Router>
-      
-        <main className='py-4'>
-          <Container>
-              <Route path='/product/:id' component={ProductPage} />
-              <Route exact path='/' component={HomePage} />
-          </Container>
-        </main>
-      
+      <Header />
+      <main className='py-4'>
+        <Container>
+          <Route path='/product/:id' component={ProductPage} />
+          <Route exact path='/' component={HomePage} />
+        </Container>
+      </main>
+      <Footer />
+
 
     </Router>
   )
