edbebf58ab8f20b941574012b021c8e98e9b3d66
Karthik Madheswaran <krithikb4u@gmail.com>
2022-05-06 11:30:29 +0100

    back end user authentication commit2

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const uSchema = mongoose.Schema({
     name:{
@@ -24,6 +25,22 @@ const uSchema = mongoose.Schema({
     timestamps: true
 })
 
+uSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
+
+uSchema.pre('save',async function (next){
+    if(!this.isModified('password')){
+        next()
+    }
+    const salt =await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt )
+
+    
+})
+
 const User = mongoose.model('User', uSchema)
+
+
 
 export default User
