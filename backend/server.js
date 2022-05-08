2e047207c92fafcb236b162df1a39318c6cea077
Karthik Madheswaran <krithikb4u@gmail.com>
2022-05-08 10:55:57 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,42 @@
 import express from 'express'
 import dotenv from 'dotenv'
-// import products from './data/products.js'
 import connectDB from './config/db.js'
-
+import path from 'path'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
 connectDB()
 
-//const express = require('express')
-//const dotenv = require('dotenv')
 const app = express()
 
 app.use(express.json())
 
-//const products = require('./data/products')
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'/frontend/build')))
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
+
 
 app.use(notFound)
 
